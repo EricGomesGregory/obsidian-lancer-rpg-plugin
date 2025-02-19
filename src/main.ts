eed0d7fa14d8c3489a43518750af99597536e97b
Eric Gomes Gregory <49287457+EricGomesGregory@users.noreply.github.com>
2025-02-19 13:52:23 -0300

    Setup basics for prisma

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { Plugin } from 'obsidian';
+import { PrismaClient } from '@prisma/client';
+import { join } from 'path';
+
+import * as fs from 'fs';
 
 // Remember to rename these classes and interfaces!
 
@@ -12,14 +16,58 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
-
+	prisma: PrismaClient;
+	
 	async onload() {
 		await this.loadSettings();
 
+		await this.initDatabase();
+	}
+
+	async initDatabase() {
+		if (this.manifest.dir == undefined) {
+			console.error("[DATABASE] init: Manifest directory not found");
+			return;
+		}
+
+		const basePath = (this.app.vault.adapter as any).getBasePath();
+		const pluginPath = join(basePath, this.manifest.dir);
+		const dbPath = join(pluginPath, 'lancer.db');
+		
+		console.log("[DATABASE] init: Trying to open dbPath...")
+		if (fs.existsSync(dbPath) == false) {
+			console.log("[DATABASE] init: Creating db from dbPath=" + `${this.manifest.dir}\\lancer.db`)
+			fs.writeFileSync(dbPath, "");
+		} else {
+			console.log("[DATABASE] init: Open db in dbPath=" + `${this.manifest.dir}\\lancer.db`)
+		}
+
+		this.prisma = new PrismaClient({
+      datasources: {
+        db: { url: `file:${dbPath}` },
+      },
+			log: ['info']
+    });
+
+		console.log("creating base table")
+		await this.prisma.contentPackManifest.createMany({data: [
+			{
+				id: 0,
+				name: "test",
+				description: "test",
+				author: "test",
+				version: "",
+				website: "",
+				image_url: ""
+			}
+		]});
+
+		let manifests = await this.prisma.contentPackManifest.findMany();
+		console.log(manifests)
 	}
 
 	onunload() {
-
+		this.prisma.$disconnect();
 	}
 
 	async loadSettings() {
